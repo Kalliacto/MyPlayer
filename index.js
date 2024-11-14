7156f471245abfc24789e1c18c5329a208c65d59
Karina Boronina <Gubashina@yandex.ru>
2024-11-14 13:48:11 +0300

    создание функций: renderElement, renderTrack, renderPlaylistHeader, renderPlyList

---
+++ index.js
@@ -0,0 +1,176 @@
+let playList = {
+    title: 'My Hits',
+    coverImageUrl: './img/playListImg1.png',
+    tracks: [
+        {
+            id: '1',
+            artist: 'The weeknd',
+            track: 'Save your tears',
+            posterUrl: 'img/Foto_1.jpg',
+            src: 'https://s2.deliciouspeaches.com/get/cuts/a9/a0/a9a08ef865d16bc4ad43fb6f342e1107/47971400/Wellbad_-_Save_Your_Tears_for_the_Well_b128f0d208.mp3',
+        },
+        {
+            id: '2',
+            artist: 'Imagine Dragons',
+            track: 'Follow You',
+            posterUrl: 'img/Foto_2.jpg',
+            src: 'https://cdn4.deliciouspeaches.com/get/cuts/65/e4/65e47dda18445ddc3a54688b35597da9/75941365/Imagine_Dragons_-_Follow_You_b128f0d176.mp3',
+        },
+        {
+            id: '3',
+            artist: 'Tove Lo',
+            track: 'How Long',
+            posterUrl: 'img/Foto_3.jpg',
+            src: 'https://cdn21.deliciouspeaches.com/get/cuts/d4/f1/d4f1d1a817125db95a6c50494aef751d/73715301/Tove_Lo_-_How_Long_b128f0d199.mp3',
+        },
+        {
+            id: '4',
+            artist: 'Tom Odell',
+            track: 'Another Love',
+            posterUrl: './img/Foto_4.jpg',
+            src: 'https://ds.cdn4.deliciouspeaches.com/get/cuts/5a/d7/5ad797429442d9b404f3ad0b2fdfa242/47971875/Tom_Odell_-_Another_Love_b128f0d251.mp3',
+        },
+        {
+            id: '5',
+            artist: 'Lana Del Rey',
+            track: 'Born To Die',
+            posterUrl: 'img/Foto_5.jpg',
+            src: 'https://s2.deliciouspeaches.com/get/music/20111209/Lana_Del_Rey_-_Born_to_die_1553565.mp3',
+        },
+        {
+            id: '6',
+            artist: 'Adele',
+            track: 'Hello',
+            posterUrl: 'img/Foto_6.jpg',
+            src: 'https://ds.cdn4.deliciouspeaches.com/get/cuts/b3/1d/b31d2277e42a0c4bab39501f323188f6/30412886/Adele_-_Hello_b128f0d295.mp3',
+        },
+        {
+            id: '7',
+            artist: 'Tom Odell',
+            track: "Can't Pretend",
+            posterUrl: 'img/Foto_7.jpg',
+            src: 'https://ds.cdn8.deliciouspeaches.com/get/cuts/ed/e9/ede93c6cdf95cf029391169fd147d107/47971881/Tom_Odell_-_Cant_Pretend_b128f0d222.mp3',
+        },
+        {
+            id: '8',
+            artist: 'Lana Del Rey',
+            track: 'Young And Beautiful',
+            posterUrl: 'img/Foto_8.jpg',
+            src: 'https://s2.deliciouspeaches.com/get/cuts/89/d3/89d3dbb6164dd1658904e8dfff01e46a/48096974/Lana_Del_Rey_-_Young_And_Beautiful_b128f0d236.mp3',
+        },
+        {
+            id: '9',
+            artist: 'Adele',
+            track: 'Someone Like You',
+            posterUrl: 'img/Foto_9.jpg',
+            src: 'https://ds.cdn5.deliciouspeaches.com/get/cuts/45/0b/450bce985623dd2f681ef5cba4025dc9/47835372/Adele_-_Someone_Like_You_b128f0d285.mp3',
+        },
+        {
+            id: '10',
+            artist: 'Imagine Dragons',
+            track: 'Natural',
+            posterUrl: 'img/Foto_10.jpg',
+            src: 'https://ds.cdn19.deliciouspeaches.com/get/cuts/40/6e/406e84c0bb04c139e6a54feaa03ced96/57429538/Imagine_Dragons_-_Natural_b128f0d190.mp3',
+        },
+        {
+            id: '12',
+            artist: 'Madonna',
+            track: 'Frozen',
+            posterUrl: 'img/Foto_12.jpg',
+            src: 'https://ds.cdn6.deliciouspeaches.com/get/cuts/b7/37/b737f4488066db67faa05084209cca54/48094475/Madonna_-_Frozen_b128f0d367.mp3',
+        },
+        {
+            id: '13',
+            artist: 'Smash Into Pieces',
+            track: 'Broken Parts',
+            posterUrl: 'img/Foto_13.jpg',
+            src: 'https://s2.deliciouspeaches.com/get/cuts/fa/01/fa01906cd0ad0091db62fd5ffe1fc116/73134678/Smash_Into_Pieces_-_Broken_Parts_b128f0d211.mp3',
+        },
+        {
+            id: '14',
+            artist: 'Aviators',
+            track: 'Our Little Horror Story',
+            posterUrl: 'img/Foto_14.jpg',
+            src: 'https://s2.deliciouspeaches.com/get/cuts/9d/5c/9d5cf78fa194ef1ed907c48ec1d307c7/62922294/Aviators_-_Our_Little_Horror_Story_b128f0d314.mp3',
+        },
+        {
+            id: '15',
+            artist: 'Poets Of The Fal',
+            track: 'My Dark Disquiet',
+            posterUrl: 'img/Foto_15.jpg',
+            src: 'https://s2.deliciouspeaches.com/get/cuts/65/69/6569764091af4dd644b98e98107066e4/59405978/Poets_Of_The_Fall_-_My_Dark_Disquiet_b128f0d310.mp3',
+        },
+        {
+            id: '16',
+            artist: 'Nephew',
+            track: 'Focus On The Sound',
+            posterUrl: 'img/Foto_16.jpg',
+            src: 'https://cdn4.deliciouspears.com/load/237548794/Nephew_-_Focus_On_The_Sound_(musmore.org).mp3',
+        },
+        {
+            id: '17',
+            artist: 'A Life Divided',
+            track: 'The Most Beautiful Black',
+            posterUrl: 'img/Foto_17.jpg',
+            src: 'https://s2.deliciouspeaches.com/get/cuts/a8/5a/a85aee92f1901296025d89b04f566bea/64292727/A_Life_Divided_-_The_Most_Beautiful_Black_b128f0d223.mp3',
+        },
+        {
+            id: '18',
+            artist: 'Five Finger Death Punch',
+            track: 'M.I.N.E (End This Way)',
+            posterUrl: 'img/Foto_18.png',
+            src: 'https://ds.cdn15.deliciouspeaches.com/get/cuts/be/af/beaf4820567537bc19fb5fc156db7379/60107453/Five_Finger_Death_Punch_-_MINE_End_This_Way_b128f0d246.mp3',
+        },
+        {
+            id: '19',
+            artist: 'Babel',
+            track: 'Gustavo Bravetti',
+            posterUrl: 'https://static.hitmcdn.com/covers/a/0e4/e70/708686.jpg',
+            src: 'https://cdn8.deliciouspeaches.com/get/cuts/84/d1/84d1d2a6df91a8e6fcd6fc4b62346551/72227705/Gustavo_Bravetti_-_Babel_b128f0d128.mp3',
+        },
+    ],
+};
+
+function renderElement(el, data) {
+    const palyListElement = document.createElement(`${el}`);
+
+    if (!!data) {
+        if (el === 'img' || el === 'audio') {
+            palyListElement.src = data;
+        } else {
+            palyListElement.append(data);
+        }
+    }
+
+    return palyListElement;
+}
+
+function renderTrack(dataTrack) {
+    const liElement = renderElement('li');
+
+    liElement.append(renderElement('span', dataTrack.artist), ` - `, renderElement('span', dataTrack.track));
+    liElement.append(renderElement('img', dataTrack.posterUrl));
+
+    const audioElement = renderElement('audio', dataTrack.src);
+    liElement.append(audioElement);
+    audioElement.controls = true;
+
+    return liElement;
+}
+function renderPlaylistHeader(inputPlaylistForRendering) {
+    document.body.appendChild(renderElement('h2', inputPlaylistForRendering.title));
+    document.body.append(`Всего треков в плейлисте: `, renderElement('span', inputPlaylistForRendering.tracks.length));
+    document.body.appendChild(renderElement('img', inputPlaylistForRendering.coverImageUrl));
+}
+function renderPlyList(playlistForRendering) {
+    renderPlaylistHeader(playlistForRendering);
+    let ulElement = renderElement('ul');
+
+    playlistForRendering.tracks.forEach((item) => {
+        ulElement.append(renderTrack(item));
+    });
+
+    document.body.append(ulElement);
+}
+
+renderPlyList(playList);
